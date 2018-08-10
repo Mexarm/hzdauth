75a6b17ecf602f9a9d798c177c1dc345f473e249
www-data <www-data@herzudigital.com>
2018-08-09 19:11:03 -0500

    first commit

---
+++ routes_py___make_hzdauth_default_app.py
@@ -0,0 +1,12 @@
+#copy this file to web2py root directory and rename it to routes.py to make dds the default app
+routers = dict( 
+    BASE = dict( 
+        default_application='hzdauth' 
+    ) 
+) 
+
+#routes_in = [
+#    ('/hzdauth/default/usr', '/hzdauth/default/usr',          
+#     dict(web2py_disable_session=True))
+#]
+
